3941b76d86f05796bb2345b8523a08ab05bf7319
Python Full-stack <koses910@gmail.com>
2024-10-21 12:49:18 +0530

    first commit

---
+++ append.py
@@ -0,0 +1,57 @@
+# lis = [23,45,6,7,88]
+# lis.append(10)
+# print(lis)
+
+
+# lis.append(["mango","banana"])
+# print(lis)
+# print(lis[6])
+# print(lis[6][0])
+# print(lis[6][-1])
+
+# lis = []
+# print(lis)
+# lis.append(["mango","banana"])
+# lis.append(["200/ky","50/dozen"])
+# print(lis)
+
+# lis[0].append(["apple"])
+# print(lis)
+# lis[1].append(["450/kg"])
+# print(lis)
+
+#----------------------------------Extend lis.extend(sequence)-----------------
+# lis = ['mango', 'banana', 'apple']
+# print(lis)
+# #lis.extend
+# lis.extend(["amrud","pinapple"])
+# print(lis)
+#-----------------------------------------------------------------------
+#insert(index,eliment)
+# lis = ['mango', 'banana', 'apple']
+# print(lis)
+# lis.insert(0,"pineapple")
+# print(lis)
+#----------------------------------------
+# lis = ['mango', 'banana', 'apple']
+# print(lis)
+# lis.insert(1,"pineapple")
+# lis.insert(0,"kela")
+# print(lis)
+#----------------------------------------------------
+# lis = ['mango', 'banana', 'apple']
+# print(lis)
+# lis1 = [23,4,-5,6,77]
+# m = max(lis1)
+# print(m)
+
+# m = max(lis)
+# print(m)
+#---------------------------------------------
+lis = ['mango', 'banana', 'apple']
+print(lis)
+lis1 = [23,4,-5,6,77]
+m = len(lis1)
+print(m)
+
+
